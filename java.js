89491a6928aa4fa9399c440fe3c18622bd96ef5f
Lakshmi P Nair <111734962+Lakshmi-Pramod@users.noreply.github.com>
2023-07-07 02:46:04 +0530

    Update java.js

--- java.js
+++ java.js
@@ -19,3 +19,19 @@ sign_up_btn2.addEventListener("click",() =>{
 sign_in_btn2.addEventListener("click",() =>{
     container.classList.remove("sign_up_mode2");
 });
+function validcheck(){
+    var isValidEmail= /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(document.getElementById("email").value);
+    var isNotEmptyPassword = document.getElementById("pass1").value!=="";
+    var isPasswordEqual = document.getElementById("pass1").value===document.getElementById("pass2").value;
+    var isNotEmptyusername = document.getElementById("Username").value!=='';
+    var isNotEmptyemail = document.getElementById("email").value!=='';
+    return isNotEmptyPassword && isPasswordEqual && isNotEmptyusername && isNotEmptyemail && isValidEmail;
+
+}
+
+function validcheck1(){
+    var isNotEmptyusername = document.getElementById("uname").value!=='';
+    var isNotEmptyPassword = document.getElementById("pass0").value!=="";
+    return isNotEmptyPassword && isNotEmptyusername;
+
+}
